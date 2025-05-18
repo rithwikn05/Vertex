d0f767d2b45b0dab8b20ab8d795fd4fd215b2a92
Rithwik Nukala <rithwikn05@gmail.com>
2025-05-17 18:56:30 -0700

    creating initial entry point

---
+++ src/Vertex/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef VX_PLATFORM_WINDOWS
+	#ifdef VX_BUILD_DLL
+		#define VERTEX_API __declspec(dllexport)
+	#else
+		#define VERTEX_API __declspec(dllimport)
+
+	#endif
+#else
+	#error Vertex only support Windows!
+#endif
